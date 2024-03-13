476361e7bdeab037670bc4ab3359f5cffb2868a3
Bogdan <bmonote@normandiewebschool.fr>
2024-03-14 00:47:25 +0100

    Fix: Front - Request - Correction sur les requêtes API. Fonction d'aide pour ajouter le token à chaque requête pour s'assurer de l'authentification.

--- Front/src/services/ApiService.js
+++ Front/src/services/ApiService.js
@@ -1,36 +1,71 @@
 import redaxios from 'redaxios';
 
+function getAuthHeaders() {
+    const token = localStorage.getItem('authToken');
+    return {
+        'Authorization': token ? `Bearer ${token}` : undefined, // Only set Authorization if token exists
+        'Content-Type': 'application/json'
+    };
+}
 const ApiService = {
     init() {
         // Set the base URL once during initialization
         redaxios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
-        // Setup token if available
-        const token = localStorage.getItem('authToken');
-        if (token) {
-            redaxios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-        }
+
+        // Ensure headers and common objects are initialized
+        // if (!redaxios.defaults.headers) {
+        //     redaxios.defaults.headers = {};
+        // }
+        // if (!redaxios.defaults.headers.common) {
+        //     redaxios.defaults.headers.common = {};
+        // }
+
+        // this.setAuthHeader();
     },
 
+    setAuthHeader() {
+        const token = localStorage.getItem('authToken');
+        return {
+            'Authorization': token ? `Bearer ${token}` : undefined, // Only set Authorization if token exists
+            'Content-Type': 'application/json'
+        };
+    },
 
     get(resource) {
-        return redaxios.get(resource);
+        console.log(redaxios.defaults)
+        return redaxios.get(resource,{ headers: getAuthHeaders() });
     },
 
     post(resource, data) {
-        const completeUrl = import.meta.env.VITE_API_BASE_URL + resource;
-        redaxios.defaults.baseURL = completeUrl
-        console.log(completeUrl);
-        return redaxios.post(completeUrl, data);
+        return redaxios.post(resource, data, { headers: getAuthHeaders() });
+    },
+
+    put(resource, data) {
+        return redaxios.put(resource, data, { headers: getAuthHeaders() });
+    },
+
+    delete(resource) {
+        return redaxios.delete(resource,{ headers: getAuthHeaders() });
     },
 
     async login(resource, data) {
-        const completeUrl = import.meta.env.VITE_API_BASE_URL + resource;
-        return await redaxios.post(completeUrl, data, {
+        // this.init()
+        const response = await redaxios.post(resource, data, {
             headers: {
-                'Content-Type': 'application/json',
-                // Include other headers here if needed
+                'Content-Type': 'application/json'
             }
         });
+
+        // if (response.data.token) {
+        //     this.setAuthHeader();
+        // }
+        return response;
+    },
+
+    logout() {
+        // localStorage.removeItem('authToken');
+        delete redaxios.defaults.headers.common['Authorization'];
+        // You might want to redirect the user to the login page or perform other cleanup actions here.
     },
 
 
